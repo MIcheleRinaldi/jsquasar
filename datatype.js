513c215598ac513955a90d24d0208e8d2e8eef96
iMac <iMac@iMac-di-iMac-121.local>
2014-01-17 09:43:40 +0100

    datatype and function samples

---
+++ datatype.js
@@ -0,0 +1,50 @@
+/*
+ * Questo è un blocco per gli appunti JavaScript.
+ *
+ *Inserire del codice JavaScript, fare clic con il tasto destro o selezionare dal menu Esecuzione:
+ * 1. Esegui per eseguire il codice selezionato (Cmd-R)
+ * 2. Analizza per aprire una finestra di analisi oggetto sul risultato (Cmd-I)
+ * 3. Visualizza per inserire il risultato come un commento dopo il testo selezionato. (Cmd-L)
+ */
+
+var isMan, isLoggedin;
+
+isMan = true;
+isLoggedin = 1;
+
+if(isMan == isLoggedin){
+    
+    console.log('i\'m in');
+
+}
+
+if(isMan === isLoggedin){
+    
+    console.log('how it\'s possible');
+
+}
+
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean(100));
+// console.log(Number(false));
+// console.log(Number(true));
+// console.log('===============');
+
+console.log(Number('1'));
+console.log(Number('giorgio'));
+console.log(100 + '2');
+console.log('bella' + 'italia');
+var name = 'giorgio', surname = 'natili';
+console.log('welcome ' + name + ' ' + surname);
+
+// console.log('===============');
+
+console.log(typeof ('1'));
+console.log(typeof (true));
+console.log(typeof ([]));
+console.log(typeof ({}));
+console.log(typeof (function(){}));
+
+
+
